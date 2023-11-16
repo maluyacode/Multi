f83cce0e459eb0429dca17cc9983b50e121797bc
airanicole <airanicole.amodia@tup.edu.ph>
2023-11-16 16:39:00 +0800

    amodia.sh, adlawan.sh

---
+++ adlawan.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config user.name maluyacode
+git config user.email adlawandavemerc98@gmail.com
---
+++ amodia.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config user.name airanicole
+git config user.email airanicole.amodia@tup.edu.ph
